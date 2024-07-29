4c2731b2204fcb8ba04dc368371e6de1af505180
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-07-29 10:54:06 -0600

    fine tuning input flexibility with reducer

--- Project/Frontend/src/places/pages/NewPlace.js
+++ Project/Frontend/src/places/pages/NewPlace.js
@@ -4,7 +4,12 @@ import Input from "../../shared/components/FormElements/Input";
 const NewPlace = () => {
 	return (
 		<form className="place-form">
-			<Input element="input" type="text" label="Title"></Input>
+			<Input
+				element="input"
+				type="text"
+				label="Title"
+				errorText="Please enter a valid title."
+			/>
 		</form>
 	);
 };
