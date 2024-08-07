2853b76529f4851df91ef9c05d743e65be6c525f
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-08-07 12:02:15 -0600

    continuing routes

--- Project/Backend/routes/places-routes.js
+++ Project/Backend/routes/places-routes.js
@@ -2,9 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	console.log("GET Request in Places");
-	res.json({ message: "It Works" });
+const DUMMY_PLACES = [
+	{
+		id: "p1",
+		title: "Empire State Building",
+		description: "Really really tall.",
+		location: { lat: 40.7484, lng: 73.9857 },
+		address: "20 W 34th St., New York, NY 10001",
+		creator: "u1",
+	},
+];
+
+router.get("/:pid", (req, res, next) => {
+	const placeId = req.params.pid;
+	const place = DUMMY_PLACES.find((p) => {
+		return p.id === placeId;
+	});
+	res.json({ place: place });
 });
 
+router.get("/user/:uid", (req, res, next) => {
+	const userId = req.params.uid;
+
+	const place = DUMMY_PLACES.find((p) => {
+		return p.creator === userId;
+	});
+	res.json({ place });
+});
 module.exports = router;
