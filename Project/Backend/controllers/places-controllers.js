a2580ac8d114828b3afd55162e70fef82571880a
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-08-09 11:33:46 -0600

    added google coord API

--- Project/Backend/controllers/places-controllers.js
+++ Project/Backend/controllers/places-controllers.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 let DUMMY_PLACES = [
 	{
 		id: "p1",
@@ -39,8 +41,21 @@ const getPlacesByUserId = (req, res, next) => {
 	res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
-	const { title, description, coordinates, address, creator } = req.body;
+const createPlace = async (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return next(
+			new HttpError("Invalid inputs. Please check your data.", 422),
+		);
+	}
+	const { title, description, address, creator } = req.body;
+
+	let coordinates;
+	try {
+		coordinates = await getCoordsForAddress(address);
+	} catch (error) {
+		return next(error);
+	}
 	const createdPlace = {
 		id: uuidv4(),
 		title,
@@ -55,6 +70,10 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlaceById = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		throw new HttpError("Invalid inputs. Please check your data.", 422);
+	}
 	const { title, description } = req.body;
 	const placeId = req.params.pid;
 
@@ -70,6 +89,9 @@ const updatePlaceById = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
 	const placeId = req.params.pid;
+	if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+		throw new HttpError("COuld not locate place using provided id", 404);
+	}
 	DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 	res.status(200).json({ message: "Place deleted" });
 };
