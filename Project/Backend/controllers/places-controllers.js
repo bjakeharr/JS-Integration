17e671dd7dd7a37e583a4f16c68f9374678005fd
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-08-14 11:23:05 -0600

    added update request

--- Project/Backend/controllers/places-controllers.js
+++ Project/Backend/controllers/places-controllers.js
@@ -90,7 +90,7 @@ const createPlace = async (req, res, next) => {
 	res.status(201).json({ place: createdPlace });
 };
 
-const updatePlaceById = (req, res, next) => {
+const updatePlaceById = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		throw new HttpError("Invalid inputs. Please check your data.", 422);
@@ -98,20 +98,37 @@ const updatePlaceById = (req, res, next) => {
 	const { title, description } = req.body;
 	const placeId = req.params.pid;
 
-	const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-	const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-	updatedPlace.title = title;
-	updatedPlace.description = description;
+	let place;
+	try {
+		place = await Place.findById(placeId);
+	} catch (err) {
+		const error = new HttpError(
+			"Something went wrong. Could not update place",
+			500,
+		);
+		return next(error);
+	}
 
-	DUMMY_PLACES[placeIndex] = updatedPlace;
+	place.title = title;
+	place.description = description;
 
-	res.status(200).json({ place: updatedPlace });
+	try {
+		await place.save();
+	} catch (err) {
+		const error = new HttpError(
+			"Something went wrong when updating place",
+			500,
+		);
+		return next(error);
+	}
+
+	res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
 	const placeId = req.params.pid;
 	if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-		throw new HttpError("COuld not locate place using provided id", 404);
+		throw new HttpError("Could not locate place using provided id", 404);
 	}
 	DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 	res.status(200).json({ message: "Place deleted" });
