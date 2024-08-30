36271a0001fdca0c1a919f16b0ca4873baf5db32
Web Development <141753914+bjakeharr@users.noreply.github.com>
2024-08-30 09:14:24 -0600

    tied edit and delete to token logic

--- Project/Frontend/src/places/components/PlaceItem.js
+++ Project/Frontend/src/places/components/PlaceItem.js
@@ -26,6 +26,10 @@ const PlaceItem = (props) => {
 			await sendRequest(
 				`http://localhost:5000/api/places/${props.id}`,
 				"DELETE",
+				null,
+				{
+					Authorization: "Bearer " + auth.token,
+				},
 			);
 			props.onDelete(props.id);
 		} catch (err) {}
